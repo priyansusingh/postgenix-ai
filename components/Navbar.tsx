885a76644f065548ccf01a58413390c652bb68a9
Priyansu Singh <priyansusingh3344@gmail.com>
2024-10-14 01:45:06 +0530

    working on navbar:

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
 'use client'
 
 import { useAuth } from "@clerk/nextjs"
+import { useEffect, useState } from "react";
 
 
 
 export function Navbar(){
-   const {userId} = useAuth()
+   const {userId} = useAuth();
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+   const [isScrolled, setIsScrolled] = useState(false);
+
+   useEffect
+
+   return <div>
+    Navbar
+   </div>
 }
