1513edd1788e7cc900a810d5189c4138c83b6fb6
Priyansu Singh <priyansusingh3344@gmail.com>
2024-10-16 09:12:08 +0530

    ready to deploy

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,8 +1,12 @@
+import { Navbar } from "@/components/Navbar";
 
-
-export default function FeaturesPage(){
-
-    return <div className="text-2xl text-blue-500 justify-center">
-        Coming Soon...
+export default function FeaturesPage() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex flex-grow justify-center items-center">
+        <h1 className="text-4xl font-bold text-blue-500">Coming Soon ...</h1>
+      </div>
     </div>
+  );
 }
